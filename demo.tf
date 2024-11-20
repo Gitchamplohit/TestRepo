72a522da9db2bbe0b97ebe6ee71639d508cb4a4f
gitchamp <lohith10101995@gmail.com>
2024-11-20 19:33:24 +0530

    test commit

---
+++ demo.tf
@@ -0,0 +1,14 @@
+resource "local_file" "demo" {
+  filename = "/root/Terraform/terraform_local/demo.txt"
+  content  = "this is my first terraform demo file"
+}
+
+resource "random_string" "rand_str" {
+  length           = "14"
+  special          = "true"
+  override_special = "!@#$%^&*><?+-/"
+}
+
+output "random_string" {
+  value = random_string.rand_str[*].result
+}
